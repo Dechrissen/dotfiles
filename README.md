b8ab41692d3c1ecac80fdbc20a5fbf49dfa71e66
Derek Andersen <derekcandersen@gmail.com>
2022-02-25 23:03:11 -0500

    start new pull-dotfiles.sh script, which will eventually allow you to pull all dotfiles from this repo to your machine

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # dotfiles
 
 This is a collection of the various dotfiles and configs I use on Arch-based systems.
+
+## scripts
+
+The `scripts` directory contains shell scripts that you can adapt and use on your own system if you wish to use my dotfiles.
+
+`pull-local-changes.sh` - Pulls local changes to dotfiles on your system into this dotfiles repo (if cloned to your machine).
---
+++ scripts/pull-dotfiles.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+# to ensure the for-loops include files starting with a .
+shopt -s dotglob
+
+# set variables to use
+dotfiles=~/dotfiles
+home=~
+config=~/.config
+
+# copy every file that is supported from ~/ into  ~/dotfiles/
+for f in $dotfiles/*
+do
+    if [[ $(basename $f) == ".config"  ]]; then
+        echo "Skipping .config"
+    else
+        cp $f $home && echo $(basename $f) "copied into ~/"
+    fi
+done
+
+# copy every file that is supported from ~/.config/ into ~/dotfiles/.config/
+for f in $dotfiles/.config/*
+do
+    cp -r $f $config && echo $(basename $f) "copied into ~/.config/"
+done
+
+echo "Finished copying dotfiles into ~/"
