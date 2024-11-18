619d55386b88624dbf3aa269014c97b1bf702220
Derek Andersen <derekcandersen@gmail.com>
2024-11-17 23:35:29 -0500

    Auto-update dotfiles repo with local changes.

--- scripts/pull-local-changes-into-repo.sh
+++ scripts/pull-local-changes-into-repo.sh
@@ -21,7 +21,7 @@ home=~
 config=~/.config
 
 # pipe-delimited string of dotfiles that this script supports/maintains
-supported=".bashrc|.nanorc|.alacritty.yml|xfce4|neofetch|micro|polybar|nitrogen|rofi|bspwm|sxhkd|dunst|picom.conf|Thunar"
+supported=".bashrc|.nanorc|.alacritty.yml|xfce4|neofetch|micro|polybar|nitrogen|rofi|bspwm|sxhkd|dunst|picom.conf|Thunar|sakura|.remarkable"
 
 # copy every file that is supported from ~/ into  ~/dotfiles/
 for f in $home/*
